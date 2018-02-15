7f7edd2d4742b275ef28631e77fd2b26d71e1194
Tanel Vahisalu <tanel.vahisalu@gmail.com>
2018-02-15 12:44:26 +0100

    validations are working

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,5 @@
-class Cocktail < ApplicationRecord
+class Ingredient < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :doses, dependent: :destroy
-  has_many :ingredients, through: :doses
+  has_many :cocktails, through: :doses
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180215112145) do
+ActiveRecord::Schema.define(version: 20180215113815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "cocktails", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "doses", force: :cascade do |t|
+    t.string "description"
+    t.integer "cocktail_id"
+    t.integer "ingredient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ingredients", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
